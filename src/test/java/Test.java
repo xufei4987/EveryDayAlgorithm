a4e8ec60d1a17bd816b321344e93dab8b531d69f
youxu <371500069@qq.com>
2020-06-06 16:43:23 +0800

    整数反转

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,5 +3,7 @@ public class Test {
         String s = "112233";
         String substring = s.substring(1, 3);
         System.out.println(substring);
+        System.out.println(-3%5);
+        System.out.println(Integer.MIN_VALUE);
     }
 }
