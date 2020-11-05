3dd79f3c27491709c7867bb4e7b76704cff72468
youxu <371500069@qq.com>
2020-11-06 00:20:28 +0800

    用一维数组优化求最长公共子串算法

--- src/main/java/algorithm1/dp/LongestCommonSubstring.java
+++ src/main/java/algorithm1/dp/LongestCommonSubstring.java
@@ -5,10 +5,46 @@ package algorithm1.dp;
  */
 public class LongestCommonSubstring {
     public static void main(String[] args) {
-        System.out.println(lcs("ABCD", "BABC"));
+        System.out.println(lcs2("ABCD", "BABC"));
     }
 
-    private static int lcs(String str1, String str2) {
+    /**
+     * 用一维数据进行优化
+     * @param str1
+     * @param str2
+     * @return
+     */
+    private static int lcs2(String str1, String str2) {
+        if (str1 == null || str1.length() < 1) return 0;
+        if (str2 == null || str2.length() < 1) return 0;
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        char[] rowChars = chars1;
+        char[] colChars = chars2;
+        if (chars1.length < chars2.length){
+            rowChars = chars2;
+            colChars = chars1;
+        }
+
+        int[] dp = new int[colChars.length + 1];
+        int max = 0;
+        for (int row = 1; row <= rowChars.length; row++) {
+            int cur = 0;
+            for (int col = 1; col <= colChars.length; col++) {
+                int leftTop = cur;
+                cur = dp[col];
+                if (rowChars[row - 1] != colChars[col - 1]){
+                    dp[col] = 0;
+                }else {
+                    dp[col] = leftTop + 1;
+                    max = Math.max(max, dp[col]);
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int lcs1(String str1, String str2) {
         if (str1 == null || str1.length() < 1) return 0;
         if (str2 == null || str2.length() < 1) return 0;
         char[] chars1 = str1.toCharArray();
